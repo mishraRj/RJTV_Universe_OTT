8dcc8af45cca5acba261130f40dc954f2e0c45af
Rajat Mishra <rajatmishra392003@gmail.com>
2024-10-26 19:12:11 +0530

    Added responsiveness

---
+++ Home_Page/Genre_Page/navBar.js
@@ -0,0 +1,7 @@
+let hamBurger = document.querySelector(".btn_one");
+let cross = document.querySelector(".btn_two");
+let navBar = document.getElementsByClassName(".nav_bar");
+
+hamBurger.addEventListener("click", () =>{
+    console.log("clicked");
+});
